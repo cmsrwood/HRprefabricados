c5648b6d6275b471c776c9c2f7376fe2ae8cd58b
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-08-20 17:37:14 -0500

    img

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 export default function Index() {
     return (
         <div className='container'>
-            
-        </div>
+            <div className="row align-items-center">
+                <div className="col">
+                    <h1 className="display-3 rojo fw-bold"> HR Prefabricados</h1>
+                    <p className="fs-4 my-5 opacidad">En HR Prefabricados diseñamos y construimos viviendas prefabricadas personalizadas, adaptadas a tus necesidades y presupuesto.</p>
+                </div>
+                <div className="col">
+                    <img src="/index_foto.jpg" alt="logo" className="img-fluid" />
+                </div>
+            </div>
+        </div >
     )
 }
