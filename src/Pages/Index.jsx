89ccdeef745e429784f0602e06a828683ed5c571
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-08-21 16:45:24 -0500

    sin hola

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -4,12 +4,19 @@ import { Link } from 'react-router-dom'
 export default function Index() {
     return (
         <div className=''>
-            <div className="container-fluid py-5">
-                <div className="text-center">
-                    <h1 className="display-3 text-danger fw-bold"> HR Prefabricados</h1>
-                    <p className="fs-4 my-5">En HR Prefabricados diseñamos y construimos viviendas prefabricadas personalizadas, adaptadas a tus necesidades y presupuesto.</p>
-                    <Link className='btn btn-danger fs-5' target='_blank' to="https://wa.me/573134549961">Contactanos</Link>
+            <div className="container-fluid py-5 ">
+                <div className="text-center pb-5 my-5 row justify-content-center align-items-center">
+                    <div className="col">
+                        <h1 className="display-3 fw-bold text-danger"> HR Prefabricados</h1>
+                        <p className="fs-4 my-5">En HR Prefabricados diseñamos y construimos viviendas prefabricadas personalizadas, adaptadas a tus necesidades y presupuesto.</p>
+                    </div>
+                    <div className="col">
+                        <Link className='btn btn-dark fs-5' target='_blank' to="https://wa.me/573134549961">Contactanos</Link>
+                    </div>
                 </div>
+            </div>
+            <div className="">
+                <h2 className='text-center'> Nosotros </h2>
             </div>
         </div >
     )
