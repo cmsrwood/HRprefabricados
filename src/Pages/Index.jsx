a505c5155bbd77d935cfdbd72314b8a2e722f8c2
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-08-21 16:31:35 -0500

    contactanos

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Swiper from '../Pages/plantilla/Swiper'
 
 export default function Index() {
     return (
         <div className=''>
-            <Swiper />
+            <div className="container-fluid py-5">
+                <div className="text-center">
+                    <h1 className="display-3 text-danger fw-bold"> HR Prefabricados</h1>
+                    <p className="fs-4 my-5">En HR Prefabricados diseñamos y construimos viviendas prefabricadas personalizadas, adaptadas a tus necesidades y presupuesto.</p>
+                    <Link className='btn btn-danger fs-5' target='_blank' to="https://wa.me/573134549961">Contactanos</Link>
+                </div>
+            </div>
         </div >
     )
 }
