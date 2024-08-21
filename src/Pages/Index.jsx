b5cff3c8d52fe00064c19b29544a2d4ea1a7fdbc
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-08-20 20:27:19 -0500

    boton

--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Index() {
     return (
         <div className='container'>
             <div className="row align-items-center">
                 <div className="col">
-                    <h1 className="display-3 rojo fw-bold"> HR Prefabricados</h1>
-                    <p className="fs-4 my-5 opacidad">En HR Prefabricados diseñamos y construimos viviendas prefabricadas personalizadas, adaptadas a tus necesidades y presupuesto.</p>
+                    <h1 className="display-2 rojo fw-bold"> HR Prefabricados</h1>
+                    <p className="fs-4 my-5 opacidad">Descubre las ventajas de las casas prefabricadas: rapidez, eficiencia y personalización.</p>
+                    <Link className='btn btn-danger fs-5'>Contactanos</Link>
                 </div>
                 <div className="col">
                     <img src="/index_foto.jpg" alt="logo" className="img-fluid" />
