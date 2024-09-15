b37693df504d600281bac75d0c45560f77d4860f
mikiweys <114179857+mikiweys@users.noreply.github.com>
2024-09-15 15:45:48 -0500

    redirigir a index.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: './index.html',
+    },
+  },
+  server: {
+    historyApiFallback: true, // Esto asegura que el servidor redirige las rutas al index.html
+  },
+});
