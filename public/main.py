d66c2d777fdf2b92d1b6f4efe490aa7fb2fca811
mikiweys <114179857+cmsrwood@users.noreply.github.com>
2024-09-17 21:37:37 -0500

    python para renombrar las iamgenes de la carpeta

--- public/main.py
+++ public/main.py
@@ -2,10 +2,8 @@ import os
 import glob
 
 def renombrar_imagenes(directorio, nuevo_nombre_base):
-    # Busca todas las imágenes jpg en el directorio
     imagenes = glob.glob(os.path.join(directorio, "*.jpg"))
     
-    # Itera sobre las imágenes encontradas y las renombra
     for idx, imagen in enumerate(imagenes, start=1):
         extension = os.path.splitext(imagen)[1]
         nuevo_nombre = f"{nuevo_nombre_base}_{idx}{extension}"
@@ -13,7 +11,6 @@ def renombrar_imagenes(directorio, nuevo_nombre_base):
         os.rename(imagen, nuevo_path)
         print(f"{imagen} renombrado a {nuevo_path}")
 
-# Usar la función
-directorio_imagenes = "./public/proyectos/1"  # Cambia esta ruta a la carpeta deseada
-nuevo_nombre_base = "imagen"  # Nombre base para las imágenes
+directorio_imagenes = "./public/proyectos/1" 
+nuevo_nombre_base = "imagen"
 renombrar_imagenes(directorio_imagenes, nuevo_nombre_base)
